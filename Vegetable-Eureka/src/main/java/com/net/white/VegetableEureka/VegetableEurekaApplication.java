c95c601c54a8842e32e784fd945bcc028075f95d
wxijun <wxj027@163.com>
2018-12-17 11:03:40 +0800

    first commit

---
+++ Vegetable-Eureka/src/main/java/com/net/white/VegetableEureka/VegetableEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.net.white.VegetableEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetableEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetableEurekaApplication.class, args);
+	}
+
+}
+
